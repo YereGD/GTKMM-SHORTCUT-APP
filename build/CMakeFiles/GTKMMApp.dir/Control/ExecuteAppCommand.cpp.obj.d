209693ac86f6d39c0e44b0153163b7d620746f54
Yeremay <1yeremay1@gmail.com>
2026-02-13 17:58:24 +0000

    ProjectWithoutBuild

--- build/CMakeFiles/GTKMMApp.dir/Control/ExecuteAppCommand.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/GTKMMApp.dir/Control/ExecuteAppCommand.cpp.obj: \
- D:\Yeremay\Proyecto\ GTKMM\ APP\Control\ExecuteAppCommand.cpp \
- D:\Yeremay\Proyecto\ GTKMM\ APP\Control\ExecuteAppCommand.h \
- D:\Yeremay\Proyecto\ GTKMM\ APP\Control\Command.h
